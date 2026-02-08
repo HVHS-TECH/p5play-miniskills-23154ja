4be3329bc7697beb1f137e9668e947a8480f0fda
23154ja <23154ja@hvhs.school.nz>
2026-02-09 10:58:37 +1300

    spell 2 monday

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -1,7 +1,7 @@
 /*******************************************************/
 // P5.play: t01_create_sprite
 // Create a sprite
-/// Written by ???
+/// Written by Jacob
 /*******************************************************/
 	
 /*******************************************************/
@@ -10,13 +10,29 @@
 function setup() {
 	console.log("setup: ");
 
+	cnv = new Canvas(windowWidth, windowHeight);
+
+	rect = new Sprite(200, 200, 50, 50, 'd');
+
+	rect.color = 'green';
+
+	circ = new Sprite(100, 100, 20, 'd');
+
+	circ.color = 'blue';
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('white'); 
+	rect = new Sprite(200, 200, 50, 50, 'd');
+
+	rect.color = 'green';
+
+	circ = new Sprite(100, 100, 20, 'd');
+
+	circ.color = 'blue';
 }
 
 /*******************************************************/
